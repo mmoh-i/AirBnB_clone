90c28e3bb5d35db36375c67a24d3a52853488404
mmoh-i <ibrahimmusty100@yahoo.com>
2022-09-06 20:05:40 -0700

    AirBnB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,10 +12,61 @@ class FileStorage:
     __objects = {}
 
     def classes(self):
-        """All classes to b returned in the program"""
+        """Returns a dictionary of valid classes and their references."""
         from models.base_model import BaseModel
-        classes = {"BaseModel": BaseModel}
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
         return classes
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
 
     def attributes(self):
         """Returns the valid attributes and their types for classname."""
